c5556ae210ecbfc2122825817842cd01ca334ff4
Samantha Barron <sam@sambarron.me>
2023-03-06 19:00:18 -0500

    Initial commit

---
+++ install.sh
@@ -0,0 +1,30 @@
+# Import variables
+
+export $(cat conf.env | xargs)
+
+# Determine and set preferred shell
+
+CURRENT_SHELL=$(basename $SHELL)
+if [[ $(basename $SHELL) != $SHELL_PREF ]]; then
+    if [[ $(command -v $SHELL_PREF) ]]; then
+        chsh -s $(which $SHELL_PREF)
+        CURRENT_SHELL=$SHELL_PREF
+    else
+        echo "Could not find $SHELL_PREF"
+    fi
+else
+    echo "Shell is already $SHELL_PREF"
+fi
+
+# Source shell.sh to config file
+
+ln -sf "$HOME/dotfiles/shell.sh" "$HOME/.shell.sh"
+RC_PATH="$HOME/.$(basename $CURRENT_SHELL)rc"
+# Check if this line exists on $RC_PATH, if not append it
+src_cmd="[ -r ~/.shell.sh ] && source ~/.shell.sh"
+if [[ $(cat $RC_PATH | grep -w "$src_cmd") ]]; then
+    echo ""
+else
+    echo ""
+    echo $src_cmd >> $RC_PATH
+fi
---
+++ shell.sh
@@ -0,0 +1,72 @@
+# Note: there is no shebang in this script. This script sets my preferred shell
+# configuration and should be able to be sourced from any Bash-like shell or
+# from Z shell.
+
+# If we are not running interactively do not continue loading this file.
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+export PATH=/opt/homebrew/bin:$PATH
+PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+
+export $(cat "$HOME/dotfiles/conf.env" | xargs)
+
+VISUAL=vim
+EDITOR=vim
+
+# Allow us to use Ctrl+S to perform forward search, by disabling the start and
+# stop output control signals, which are not needed on modern systems.
+stty -ixon
+
+# Set a shell option but don't fail if it doesn't exist!
+safe_set() { shopt -s "$1" >/dev/null 2>&1 || true; }
+
+# Set some options to make working with folders a little easier. Note that we
+# send all output to '/dev/null' as startup files should not write to the
+# terminal and older shells might not have these options.
+safe_set autocd         # Enter a folder name to 'cd' to it.
+safe_set cdspell        # Fix minor spelling issues with 'cd'.
+safe_set dirspell       # Fix minor spelling issues for commands.
+safe_set cdable_vars    # Allow 'cd varname' to switch directory.
+
+# Uncomment the below if you want to be able to 'cd' into directories that are
+# not just relative to the current location. For example, if the below was
+# uncommented we could 'cd my_project' from anywhere if 'my_project' is in
+# the 'repos' folder.
+# CDPATH="~:~/repos"
+
+# Configure the history to make it large and support multi-line commands.
+safe_set histappend                  # Don't overwrite the history file, append.
+safe_set cmdhist                     # Multi-line commands are one entry only.
+PROMPT_COMMAND='history -a'          # Before we prompt, save the history.
+HISTSIZE=10000                       # A large number of commands per session.
+HISTFILESIZE=100000                  # A huge number of commands in the file.
+# HISTCONTROL="ignorespace:ignoredup" # Ignore starting with space or duplicates?
+# export HISTIGNORE="ls:history"     # Any commands we want to not record?
+HISTTIMEFORMAT='%F %T '            # Do we want a timestamp for commands?
+
+# Colors
+
+#if [[ $(basename $SHELL) == "zsh" ]]; then
+if [[ false ]]; then
+    autoload -U colors && colors
+    PROMPT="%F{magenta}%n%f%F{white}@%f%F{cyan}%m%f %F{magenta}%1~%f%F{white} >%f "
+else
+    PS1="\u@\h %1~ > "
+fi
+
+autoload -U colors && colors
+
+# Aliases
+
+alias ll="clear; pwd; ls -lh"
+
+function cs() {
+    cd $1
+    clear
+    pwd
+    echo ""
+    ls
+}
